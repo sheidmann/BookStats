9790d2cd6754534c44769410520f0d678fdda905
sheidmann <sarah.heidmann@gmail.com>
2020-08-16 11:53:53 -0400

    import GR data

--- scripts/exploreGRdata.R
+++ scripts/exploreGRdata.R
@@ -1,2 +1,16 @@
 # exploreGRdata.R
 
+# Sarah Heidmann
+# Created 16 Aug, 2020
+# Last modified 16 Aug, 2020
+
+# Load libraries
+library(tidyverse)
+
+##### Import the data #####
+books <- read_csv("data/goodreads_library_export_20200816.csv",
+                  col_types = cols(`Date Read`=col_date(format="%D"),
+                                   `Date Added`=col_date(format="%D")))
+
+
+      
